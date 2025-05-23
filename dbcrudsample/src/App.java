46414dc08f51293e4036e7a788eff55474d1d8af
nugrahaindie <nugraha@indietechsolutions.com>
2025-05-23 10:24:45 +0700

    create sample edit, delete and create

--- dbcrudsample/src/App.java
+++ dbcrudsample/src/App.java
@@ -10,6 +10,9 @@ public class App {
 
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
             if (conn.isValid(5)) {
+                createData(conn);
+                updateData(conn);
+                deleteData(conn);
                 readData(conn);
             }
         } catch (SQLException e) {
@@ -21,20 +24,50 @@ public class App {
         String query = "SELECT * FROM products";
         try (Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(query)) {
-            System.out.println("+------------------+------------+----------------+");
-            System.out.println("| Name             | SKU        | Price  ");
-            System.out.println("+------------------+------------+----------------+");
+            System.out.println("+------------------+----------------+----------------+");
+            System.out.println("| Name             | SKU            | Price          |");
+            System.out.println("+------------------+----------------+----------------+");
             
             while (rs.next()) {
                 String name = rs.getString("name");
                 String sku = rs.getString("sku");
                 String price = rs.getString("price");
                 
-                System.out.printf("| %-16s | %-10s | %-6s \n", 
+                System.out.printf("| %-16s | %-14s | %-14s | \n", 
                     name, sku, price);
             }
             
-            System.out.println("+------------------+------------+----------------+");
+            System.out.println("+----------------------+------------+----------------+");
+        }
+    }
+
+    private static void createData(Connection conn) throws SQLException {
+        String query = "INSERT INTO products (name, sku, price) VALUES (?, ?, ?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, "School Bag");
+            pstmt.setString(2, "SKU00012");
+            pstmt.setDouble(3, 1000000.0);
+            pstmt.executeUpdate();
+            System.out.println("Data created successfully");
+        }
+    }
+
+    private static void updateData(Connection conn) throws SQLException {
+        String query = "UPDATE products SET sku = ? WHERE id = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, "SKUACR00011");
+            pstmt.setString(2, "1");
+            pstmt.executeUpdate();
+            System.out.println("Data updated successfully");
+        }
+    }
+
+    private static void deleteData(Connection conn) throws SQLException {
+        String query = "DELETE FROM products WHERE sku = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, "SKU00012");
+            pstmt.executeUpdate();
+            System.out.println("Data deleted successfully");
         }
     }
 }
